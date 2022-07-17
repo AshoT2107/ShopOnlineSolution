8ede55bc10ba4c792bb155137619c18e9ab157ba
ashot <ashotm1999@gmail.com>
2022-07-18 03:11:27 +0500

    Add project files.

---
+++ ShopOnline.API/Entities/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopOnline.API.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Qyt { get; set; }
+    }
+}
